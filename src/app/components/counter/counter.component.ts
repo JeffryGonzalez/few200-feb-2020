7a65f387f0c3b503db766fd4ca75107256b7e147
mzpepper <nclark16@kent.edu>
2020-02-04 18:14:35 +0000

    so far so good

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppState } from 'src/app/reducers';
 import { Store } from '@ngrx/store';
+import * as actions from '../../actions/counter.actions';
 
 @Component({
   selector: 'app-counter',
@@ -19,15 +20,18 @@ export class CounterComponent implements OnInit {
     this.atStart$ = this.store.select(s => s.counter.current === 0);
   }
   increment() {
-    this.store.dispatch({ type: 'increment' });
+    // this.store.dispatch({ type: 'increment' });
+    this.store.dispatch(actions.countIncremented());
   }
 
   decrement() {
-    this.store.dispatch({ type: 'decrement' });
+    // this.store.dispatch({ type: 'decrement' });
+    this.store.dispatch(actions.countDecremented());
   }
 
   reset() {
-    this.store.dispatch({ type: 'reset' });
+    // this.store.dispatch({ type: 'reset' }); used before the actions were setup
+    this.store.dispatch(actions.countReset());
   }
 
 }
--- src/app/reducers/counter.reducer.ts
+++ src/app/reducers/counter.reducer.ts
@@ -1,4 +1,5 @@
-import { Action } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
+import * as actions from '../actions/counter.actions';
 
 export interface CounterState {
   current: number;
@@ -8,27 +9,37 @@ export const initialState: CounterState = {
   current: 0
 };
 
-export function reducer(state: CounterState = initialState, action: Action) {
-  switch (action.type) {
-    case 'increment': {
-      return {
-        current: state.current + 1
-      };
-    }
-    case 'decrement': {
-      return {
-        current: state.current - 1
-      };
-    }
-    // because the initialState is already an Object you do not put it inside curly braces like an object
-    case 'reset': {
-      return initialState;
-      // current: state.current = 0
+const myReducer = createReducer(
+  initialState,
+  on(actions.countReset, () => initialState),
+  on(actions.countIncremented, (s) => ({ current: s.current + 1 })),
+  on(actions.countDecremented, (s) => ({ current: s.current - 1 }))
+);
+
+export function reducer(state: CounterState = initialState, action: Action): CounterState {
+  return myReducer(state, action);
+
+  // before the reducers were available
+  // switch (action.type) {
+  //   case 'increment': {
+  //     return {
+  //       current: state.current + 1
+  //     };
+  //   }
+  //   case 'decrement': {
+  //     return {
+  //       current: state.current - 1
+  //     };
+  //   }
+  //   // because the initialState is already an Object you do not put it inside curly braces like an object
+  //   case 'reset': {
+  //     return initialState;
+  //     // current: state.current = 0
 
 
-    }
-    default: {
-      return state;
-    }
-  }
+  //   }
+  //   default: {
+  //     return state;
+  //   }
+  // }
 }
